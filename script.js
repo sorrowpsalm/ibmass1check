500f130a1b2fbd7674b0d49052730b7fc44a11c7
sorrowpsalm <101848750+sorrowpsalm@users.noreply.github.com>
2022-10-26 10:42:38 +0530

    testing upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// function displayMessage() {
+//     window.alert("Your user creation form has been succesfully submitted!");
+// }
+
+function displayMessage() {
+    var inputname = document.getElementById("name").value;
+    var inputmobile = document.getElementById("mobile").value;
+    var inputemail = document.getElementById("email").value;
+}
+
